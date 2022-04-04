18f330ffbba203991571341c3797f248bce6c296
franco garcia <francogarcia1331@gmail.com>
2022-04-04 18:17:33 -0300

    continuo ejer5

--- Practica2/Ejer1/ejer5.pas
+++ Practica2/Ejer1/ejer5.pas
@@ -1,7 +1,7 @@
 program ejer5;
 const
     valorAlto = 9999;
-    df = 50;
+    df = 2;
 type
     cad = string[20];
 
@@ -43,7 +43,54 @@ type
         horaDeceso: Integer;
     end;
 
+    {nacimientos y fallecimientos seran los archivos detalles}
+    nacimientos = file of rNacimiento;
+    fallecimientos = file of rFallecimiento;
 
+
+
+    rMaestro = record
+        infoNacimientos: rNacimiento;
+        infoFallecimientos: rFallecimiento;
+    end;
+
+    maestro = file of rMaestro;
+
+    detNacimientos = array [1..df] of nacimientos;
+    detFallecimientos = array [1..df] of fallecimientos;
+
+    vecNacimientos = array [1..df] of rNacimiento;
+    vecFallecimientos = array [1..df] of rFallecimiento;
+
+procedure leerNacimientos(var nac: nacimientos; var regNac: rNacimiento);
+begin
+    if (not EOF(nac)) then
+        read(nac, regNac)
+    else
+        regNac.nroPartidaNacimiento := valorAlto;
+end;
+
+procedure leerFallecimientos(var fa: fallecimientos; var regFa: rFallecimiento);
+begin
+    if (not EOF(fa)) then
+        read(fa, regFa)
+    else
+        regFa.nroPartidaNacimiento := valorAlto;
+end;
+
+procedure minimoNacimiento ();
+var
+
+begin
+
+end;
+
+procedure minimoFallecimiento();
+var
+
+begin
+
+end;
 
 //programa principal
 var
