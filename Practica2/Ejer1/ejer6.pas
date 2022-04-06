bb632cb561fc669fe252357d2b19c554c6216a31
Franco Garcia <francogarcia1331@gmail.com>
2022-04-06 11:52:43 -0300

    .

--- Practica2/Ejer1/ejer6.pas
+++ Practica2/Ejer1/ejer6.pas
@@ -26,8 +26,42 @@ type
     end;
     maestro = file of rMaestro;
 
-    
+    vecDetalles = array [1..df] of detalle;
+    vecRegistros = array [1..df] of rDetalle;
 
+procedure leerDetalle (var det: detalle; var dato: rDetalle);
+begin
+    if (not EOF(det)) then
+        read(det, dato)
+    else
+        dato.codLocalidad := valorAlto;
+end;
+
+procedure minimo (var vDet: vecDetalles; var vReg:vecRegistros; var min:rDetalle);
+var
+    i, indiceMin : integer;
+begin
+    indiceMin:=0;
+    min.codLocalidad:= valorAlto;
+    min.codCepa:= valorAlto;
+    for i:= 1 to df do begin
+        if (vReg[i].codLocalidad < min.codLocalidad) then begin
+            if (vReg[i].codCepa < min.codCepa) then begin
+                min:= vReg[i];
+                indiceMin := i;
+            end;
+        end;
+    end;
+    if (indiceMin <> 0) then
+      leerDetalle(vDet[indiceMin], vReg[indiceMin]);
+end;
+
+procedure actualizarMaestro (var mae: maestro; var vDet: vecDetalles);
+var
+
+begin
+
+end;
 
 {programa principal}
 var
