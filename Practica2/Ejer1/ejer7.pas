387a87c705b098122763a11044e71c1b2c23c1a4
Franco Garcia <francogarcia1331@gmail.com>
2022-04-06 18:18:23 -0300

    sigo ejer7

--- Practica2/Ejer1/ejer7.pas
+++ Practica2/Ejer1/ejer7.pas
@@ -1,6 +1,6 @@
 program ejer7;
 const
-
+    valorAlto = 9999;
 type
     cad = string[20];
     rMaestro = record
@@ -17,6 +17,35 @@ type
         cantVendida: integer;
     end;
     detalle = file of rDetalle;
+
+procedure leer (var det: detalle;var dato: rDetalle);
+begin
+    if (not EOF(det)) then
+        read(det,dato)
+    else
+        dato.cod:= valorAlto;
+end;
+
+procedure actualizarMaestro (var mae: maestro; var det: detalle);
+var
+    aux, rDet: rDetalle;
+begin
+    Reset(mae);
+    Reset(det);
+    leer(det, rDet);
+    while (rDet.cod <> valorAlto) do begin
+        aux.cod := rDet.cod;
+        aux.cantVendida := 0;
+        while (rDet.cod = aux.cod) do begin
+            aux.cantVendida := aux.cantVendida + rDet.cantVendida;
+            leer(det,rDet);
+        end;
+        read(mae, rMae);
+        while (rMae.cod <> aux.cod) do 
+            read(mae,rMae);
+        rMae.stockAct := rMae.stockAct - aux.cantVendida;
+    end;
+end;
 
 {programa principal}
 var
