60c0d73ad63f692f26606b20b9fe89c4d3e4e495
Franco Garcia <francogarcia1331@gmail.com>
2022-04-06 18:07:28 -0300

    inicio ejer7

--- Practica2/Ejer1/ejer7.pas
+++ Practica2/Ejer1/ejer7.pas
@@ -2,6 +2,21 @@ program ejer7;
 const
 
 type
+    cad = string[20];
+    rMaestro = record
+        cod: integer;
+        nomComercial: cad;
+        precio: real;
+        stockAct: integer;
+        stockMin : integer;
+    end;
+    maestro = file of rMaestro;
+
+    rDetalle = record
+        cod: integer;
+        cantVendida: integer;
+    end;
+    detalle = file of rDetalle;
 
 {programa principal}
 var
