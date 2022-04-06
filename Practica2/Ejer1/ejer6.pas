1c3d0b5ad9af99abbcf9d7f448043c02e0ed766c
Franco Garcia <francogarcia1331@gmail.com>
2022-04-06 15:52:00 -0300

    ejer 6 terminado

--- Practica2/Ejer1/ejer6.pas
+++ Practica2/Ejer1/ejer6.pas
@@ -91,12 +91,35 @@ begin
         rMae.cantFallecidos:= aux.cantFallecidos;
 
         seek(mae, filepos(mae)-1);
+        write(mae, rMae);
     end;
+    close(mae); {cierro el archivo maestro}
+
+    for i:= 1 to df do begin {cierro los archivos detalle}
+        close(vDet[i]);  
+    end;
+end;
+
+procedure informarLocalidades (var mae: maestro);
+var
+    rMae: rMaestro;
+begin
+    Reset(mae);
+    while (not EOF(mae)) do begin
+        read(mae, rMae);
+        if (rMae.cantActivos > 50) then begin
+            Write('La localidad: ',rMae.nomLocalidad,' con codigo ',rMae.codLocalidad,'
+            ' tiene mas de 50 casos activos actualmente');
+        end; 
+    end;
+    close(mae);
 end;
 
 {programa principal}
 var
-
+    mae: maestro;
+    vDetalles: vecDetalles;
 begin
-  
+    actualizarMaestro(mae, vDetalles);
+    informarLocalidades(mae);
 end.
