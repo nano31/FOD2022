5ce052bf5562c38ac45d0ef984a56163fb4cb693
Garcia Carril Celina <celigarcia1331@gmail.com>
2022-04-15 02:28:51 -0300

    sigo ejer 6

--- Practica3.1/ejer6.pas
+++ Practica3.1/ejer6.pas
@@ -19,12 +19,45 @@ type
     detalle = file of integer; //archivo con los codigos para actualizar el maestro
     nuevoArchivo = file of rPrenda; //archivo compactado
 
-procedure leer (var arch: fileMaestro; var dato: rPrenda);
+procedure leerMaestro (var arch: maestro; var dato: rPrenda);
 begin
     if(not EOF(arch)) then
       read(arch, dato)
     else
         dato.cod := valorAlto;
+end;
+
+procedure leerDetalle (var det: detalle; var cod: integer);
+begin
+    if (not EOF(det)) then
+        read(det,cod)
+    else
+        cod := valorAlto;
+end;
+
+procedure eliminarPrendas(var mae:maestro; var det: detalle);
+var
+    dato: rPrenda;
+    cod: integer;
+begin
+    Reset(mae);
+    Reset(det);
+    
+    leerDetalle(det,cod);
+    while (cod <> valorAlto) do begin
+        leerMaestro(mae, dato);
+        while((dato.cod <> valorAlto) and (dato.cod <> cod)) do begin
+            leerMaestro(mae, dato);
+        end;
+        if(dato.cod = cod) then begin
+            dato.stock = dato.stock * -1;
+            seek(mae, filepos(mae)-1);
+            write(mae, dato);
+        end;
+        leerDetalle(det, cod);
+    end;
+    close(mae);
+    close(det);
 end;
 
 
