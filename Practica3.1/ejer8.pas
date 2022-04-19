8635db740dbd4e96655b37ea8d0b3d48ddc45e81
Franco Garcia <francogarcia1331@gmail.com>
2022-04-19 10:58:28 -0300

    .

--- Practica3.1/ejer8.pas
+++ Practica3.1/ejer8.pas
@@ -13,22 +13,47 @@
 
     maestro = file of rDistro;
 
-function existeDistribucion(nombreDistro: cad): boolean; 
+function existeDistribucion(nombreDistro: cad; var arch: maestro): boolean; 
 //devuelve true o false dependiendo si la distro 
 //buscada existe o no
 var
-
+    distro: rDistro;
+    encontre: Boolean;
 begin
+    Reset(arch);
+    encontre:= false;
 
+    while (not EOF(arch) and not encontre) do begin
+        leer(arch, distro);
+        if (distro.nombre = nombreDistro) then
+            encontre:= true;
+    end;
 end;
 
 procedure altaDistribucion(var mae: maestro);
 //realiza un alta en el archivo, es decir,
 //agrega una nueva distribucion al mismo
 var
-
+    nuevaDistro, cabecera: rDistro;
 begin
+    Reset(mae);
+    leerDistro(nuevaDistro);
 
+    if(not existeDistribucion) then begin
+        leerArch(arch, cabecera);
+        if (cabecera.cantDesarrolladores < 0) then begin
+            seek(arch, cabecera.cantDesarrolladores*-1);
+            read(arch, cabecera);
+            seek(arch, filePos(arch)-1);
+            write(arch,nuevaDistro);
+            seek(arch,0);
+            write(arch,cabecera);
+        end
+        else
+            WriteLn('No hay espacio dentro del archivo');
+    end
+    else
+        WriteLn('La distribucion ingresada ya existe en el archivo');
 end;
 
 procedure bajaDistribucion(var mae: maestro; nombreDistro: cad);
